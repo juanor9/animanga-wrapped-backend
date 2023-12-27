d06d31d49946257cd48e012dc6479a822264b720
Juan Camilo Orjuela <orjuela9@gmail.com>
2023-12-27 11:19:57 -0500

    polish console logs

--- src/upload/cloudinary/cloudinary.controller.mjs
+++ src/upload/cloudinary/cloudinary.controller.mjs
@@ -1,31 +1,30 @@
-import fs from 'fs';
-import { uploadImage } from './cloudinary.services.mjs';
+import fs from "fs";
+import { uploadImage } from "./cloudinary.services.mjs";
 
-const handleUploadSingleImage = async(
-  req, res
-) => {
-  console.log("🚀 ~ file: cloudinary.controller.mjs:7 ~ req:", req)
-  
-  const {path, size} = req.file;
-  console.log("🚀 ~ file: cloudinary.controller.mjs:10 ~ path:", path)
-  console.log("🚀 ~ file: cloudinary.controller.mjs:10 ~ req.file:", req.file)
-  const {imageName, username} = req.body;
-  console.log("🚀 ~ file: cloudinary.controller.mjs:12 ~ username:", username)
-  console.log("🚀 ~ file: cloudinary.controller.mjs:12 ~ imageName:", imageName)
-  const maxSize = 1080 *1920*10;
+const handleUploadSingleImage = async (req, res) => {
+  const { path, size } = req.file;
+  console.log("🚀 ~ file: cloudinary.controller.mjs:10 ~ path:", path);
+  const { imageName, username } = req.body;
+  console.log("🚀 ~ file: cloudinary.controller.mjs:12 ~ username:", username);
+  console.log(
+    "🚀 ~ file: cloudinary.controller.mjs:12 ~ imageName:",
+    imageName
+  );
+  const maxSize = 1080 * 1920 * 10;
 
-  if(size > maxSize){
-      fs.unlinkSync(path);
-      return res.status(400).json({message: "File is too large"});
+  if (size > maxSize) {
+    fs.unlinkSync(path);
+    return res.status(400).json({ message: "File is too large" });
   }
   try {
-      const result = await uploadImage({path, imageName, username});
-      res.json(result);
+    const result = await uploadImage({ path, imageName, username });
+    res.json(result);
   } catch (error) {
-      return res.status(500).json({message: error.message});
-  } finally{
-      fs.unlinkSync(path);
+    console.log(error.message);
+    return res.status(500).json({ message: error.message });
+  } finally {
+    fs.unlinkSync(path);
   }
-}
+};
 
 export default handleUploadSingleImage;
