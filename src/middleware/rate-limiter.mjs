d50db9fa6621e71dd1e04c70dd2124b835612290
orjuela9 <orjuela9@gmail.com>
2025-10-20 00:53:55 +0000

    update users' security

---
+++ src/middleware/rate-limiter.mjs
@@ -0,0 +1,9 @@
+import rateLimit from 'express-rate-limit';
+
+export const authLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 10, // Limit each IP to 10 requests per windowMs
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  message: 'Too many requests from this IP, please try again after 15 minutes',
+});
