627711a3d439d32c61df58abb8b785186f5fa3ad
orjuela9 <orjuela9@gmail.com>
2025-10-25 15:02:16 +0000

    solve timing vulnerability issue

--- src/auth/local/local.controller.mjs
+++ src/auth/local/local.controller.mjs
@@ -1,5 +1,6 @@
 import { getUserFilter } from '../../user/user.services.mjs';
 import { signToken, handleFailedLogin, handleSuccessfulLogin } from '../auth.services.mjs';
+import bcrypt from 'bcryptjs';
 
 async function handleLogin(
   req,
@@ -10,19 +11,22 @@ async function handleLogin(
   try {
     const user = await getUserFilter({ email });
 
-    if (!user) {
+    // DUMMY HASH: to be used for timing attack prevention
+    // It must have the same format of a bcrypt hash
+    const DUMMY_HASH = '$2a$10$iE.3s5.a.CGSs4/Ie.s5.OAFx3nANiC.e13rJ8M/25.d3.yspaSpO';
+    const passwordToCompare = user ? user.password : DUMMY_HASH;
+
+    const validPassword = await bcrypt.compare(password, passwordToCompare);
+
+    if (!user || !validPassword) {
+      if (user) {
+        await handleFailedLogin(user);
+      }
       return res.status(401).json({ message: 'El correo electrónico o la contraseña son incorrectos' });
     }
 
     if (user.isActive !== true) {
       return res.status(401).json({ message: 'User is not active' });
-    }
-
-    const validPassword = await user.comparePassword(password);
-
-    if (!validPassword) {
-      await handleFailedLogin(user);
-      return res.status(401).json({ message: 'El correo electrónico o la contraseña son incorrectos' });
     }
 
     await handleSuccessfulLogin(user);
